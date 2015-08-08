fadc62dca940b6d42c82f84c85ebf905dba3bbc6
Surume <mail@sururne.com>
2015-08-08 22:43:51 +0900

    Add lib/itamae/plugin/recipe/anyenv/system.rb

---
+++ lib/itamae/plugin/recipe/anyenv/system.rb
@@ -0,0 +1,23 @@
+package "git"
+
+scheme = "git"
+scheme = node[:anyenv][:scheme] if node[:anyenv][:scheme]
+
+require "itame/plugin/recipe/anyenv"
+
+git anyenv_root do
+  repository "#{scheme}://github.com/riywo/anyenv.git"
+end
+
+git "#{anyenv_root}/plugins/anyenv-update" do
+  repository "#{scheme}://github.com/znz/anyenv-update.git"
+end
+
+directory "#{anyenv_root}/envs"
+
+node[:anyenv][:install_envs].each do |env|
+  execute "install #{env}" do
+    command "#{anyenv_init} anyenv install #{env}"
+    not_if "type #{env}"
+  end
+end
