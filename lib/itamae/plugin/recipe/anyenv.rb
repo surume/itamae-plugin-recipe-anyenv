478c68ee313dc922015d92b94e270310084d06c4
Surume <mail@sururne.com>
2015-08-08 22:13:14 +0900

    bundle gem itamae-plugin-recipe-anyenv

---
+++ lib/itamae/plugin/recipe/anyenv.rb
@@ -0,0 +1,11 @@
+require "itamae/plugin/recipe/anyenv/version"
+
+module Itamae
+  module Plugin
+    module Recipe
+      module Anyenv
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/itamae/plugin/recipe/anyenv/version.rb
@@ -0,0 +1,9 @@
+module Itamae
+  module Plugin
+    module Recipe
+      module Anyenv
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
