1884064480a230063466761a63e79068598758d7
surume <sururne@gmail.com>
2016-05-22 16:55:36 +0900

    Remove unnecessary var

--- lib/itamae/plugin/recipe/anyenv.rb
+++ lib/itamae/plugin/recipe/anyenv.rb
@@ -32,7 +32,6 @@ def anyenv_init(root_path)
   init_str =  %(export ANYENV_ROOT="#{root_path}"; )
   init_str << %(export PATH="#{root_path}/bin:${PATH}"; )
   init_str << %(eval "$(anyenv init -)"; )
-  init_str
 end
 
 def run(attributes, username = nil)
